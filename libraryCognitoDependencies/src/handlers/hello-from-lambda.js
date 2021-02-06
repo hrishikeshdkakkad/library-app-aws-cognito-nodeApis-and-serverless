62475ee62eeec5989f5d290777e912cb533942ac
Hrishikesh D Kakkad <hrishidkakkad@gmail.com>
2021-02-06 16:35:22 +0530

    Added trigger condition on lambda function to create a mapping for the user after confirmation in MongoDB

--- libraryCognitoDependencies/src/handlers/hello-from-lambda.js
+++ libraryCognitoDependencies/src/handlers/hello-from-lambda.js
@@ -2,15 +2,13 @@
  * A Lambda function that returns a static string
  */
 const AWS = require("aws-sdk")
+const axios = require("axios")
+const FormData = require("form-data")
+require('dotenv/config')
+
 
 const cognitoIdentityServiceProvider = new AWS.CognitoIdentityServiceProvider()
 exports.helloFromLambdaHandler = async (event, context, callback) => {
-    // If you change this message, you will need to change hello-from-lambda.test.js
-    console.log(event, "event from cognito")
-    const message = 'Hello from Lambda!';
-
-    // All log statements are written to CloudWatch
-    console.info(`${message}`);
 
     const params = {
         GroupName: "applicationUsers",
@@ -19,8 +17,19 @@ exports.helloFromLambdaHandler = async (event, context, callback) => {
     };
 
     try {
-        const result = await cognitoIdentityServiceProvider.adminAddUserToGroup(params).promise()
-        console.log(result)
+        await cognitoIdentityServiceProvider.adminAddUserToGroup(params).promise()
+
+        let data = new FormData()
+        data.append('username', event.userName)
+
+        let config = {
+            headers: {
+                ...data.getHeaders()
+            },
+        }
+
+        const postResult = await axios.post(process.env.API_URL, data, config)
+        console.log(postResult, "post result")
     } catch (error) {
         console.log(error)
     }
